4edc563cf8224206d1211b59a16c4baaf3fcefb8
hpgit211 <hardik007.hp68@gmail.com>
2026-09-13 23:55:25 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
-const CACHE='daily-count-v4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='daily-count-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='daily-count-v4').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const isPage=e.request.mode==='navigate' || e.request.url.endsWith('/index.html');
-  if(isPage){
-    e.respondWith(fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res;}).catch(()=>caches.match('./index.html')));
-  } else {
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;})));
-  }
+ if(e.request.method!=='GET') return;
+ const isPage=e.request.mode==='navigate'||e.request.url.endsWith('/index.html');
+ if(isPage){e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')))}
+ else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}))) }
 });
